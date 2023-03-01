823484e2c839b9ac30820e419c30f5939c89fdfb
maksi <maksimbaltak@yandex.ru>
2023-03-01 17:15:47 +0200

    add .env

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,12 @@
 import { NestFactory } from '@nestjs/core';
+import { config } from 'dotenv';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
+  config()
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  await app.listen(process.env.SERVER_PORT, process.env.SERVER_HOST, () => {
+    console.log(`server run on port ${process.env.SERVER_PORT}`)
+  });
 }
 bootstrap();
