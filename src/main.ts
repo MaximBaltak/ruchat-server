e59cc5efc38a0809ef67c0f74f0663bfecd44e62
maksi <maksimbaltak@yandex.ru>
2023-03-31 19:54:30 +0200

    add cross

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,11 @@ import {AppModule} from './app.module';
 async function bootstrap() {
     config()
     const app = await NestFactory.create(AppModule);
+    app.enableCors({
+        origin:'*',
+        methods:['GET','POST','DELETE','PATCH','PUT','OPTIONS'],
+        allowedHeaders:[ 'Content-Type','Authorization']
+    })
     await app.listen(process.env.SERVER_PORT, process.env.SERVER_HOST, () => {
         console.log(`server run on port ${process.env.SERVER_PORT}`)
     });
